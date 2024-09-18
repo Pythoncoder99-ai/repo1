d20d5436acc8275ba65ecd7e4a11ab08fa99f662
Pythoncoder00-ai <shadabforwork@gmail.com>
2024-09-18 23:55:49 +0530

    added 3 more program in file

--- while_loops.py
+++ while_loops.py
@@ -1,3 +1,4 @@
+
 #1. while loop to print python word for 5 times.
 a=('\n this is just for printing lines at multiple times.\n')
 b=a.title()
@@ -7,7 +8,7 @@ while count<5:
     print(" Hey! Iam Python....")
     count+=1
 
-a=("\n this is the next  program you're seeing downwards")
+a=("\n this is the next program that you're seeing below")
 b=a.upper()
 print(b)
 
@@ -18,7 +19,7 @@ print('\n')
 while start<=end:
     print(start)
     start+=1
-a=("\n this is the next  program you're seeing downwards")
+a=("\n this is the next  program that you're seeing below")
 b=a.upper()
 print(b)
 
@@ -29,6 +30,51 @@ end=0
 while end<=start:
     print(start)
     start-=1
-a=('''\n Thank you. You've succesfully executed today's programs!''')
+
+a=("\n this is the next  program that you're seeing below")
+b=a.upper()
+print(b)
+
+
+#Program to add all the digits in a user entered number using while loop
+num=int(input("enter the number:  "))
+sum=0
+while num!=0:
+    last_dig=num%10
+    sum+=last_dig
+    num//=10
+print(f"The added number is {sum}")
+
+a=("\n this is the next  program that you're seeing below")
+b=a.upper()
+print(b)
+
+#Program to add all the digits in the user entered number only if it is an even number using while loop.
+num=int(input("enter the number: "))
+sum=0
+while num!=0:
+    last_dig=num%10
+    if last_dig%2==0:
+        sum+=last_dig
+    num//=10
+    print(f"The added number is an even number: {sum}")
+    
+a=("\n this is the next  program that you're seeing below")
+b=a.upper()
+print(b)
+
+#Program to add each digit in a number only if the digit is even number using while loop
+num=int(input("enter the number: "))
+if num%2==0:
+    print(f"{num} is an even number, so it will processed further")
+    sum=0
+    while num!=0:
+        last_dig=num%10
+        sum+=last_dig
+        num//=10
+    print(f"And the sum is: {sum} ")
+else:
+    print(f"{num} is not an even number, so digit will not processed further")
+a=(''' \n Thank you. You've succesfully executed today's programs!''')
 b=a.upper()
 print(b)
