071c39eacd6696f5b9f8c58f0110f4305b4dc99c
Pythoncoder99-ai <shadabforwork@gmail.com>
2024-09-30 06:56:57 +0530

    add print

--- first.py
+++ first.py
@@ -1,6 +1,7 @@
 print('hello this is testing......')
 age= int(input("enter age: "))
 if age>17:
-  print("you're eligible")
+  print("you're eligible!")
 else:
   print("sorry you're not eligible!")
+  print('')
